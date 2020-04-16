9324c32f92063169a9ee9911b65d22a64f725d79
Tim Franssen <tim.franssen@nedap.com>
2020-04-16 17:06:02 +0200

    Moved game separation (rooms) stuff to its own file, made join codes 6 characters alphanumeric, fixed a lot of small live presence update things of players and games

---
+++ client/game-separation.js
@@ -0,0 +1,85 @@
+export default function gameSeparation(socket, joinCallback, leaveCallback) {
+
+  let currentGame;
+
+  const createButton = document.getElementById('create');
+  const joinForm     = document.getElementById('join');
+  const playersList  = document.getElementById('players');
+  const gamesList    = document.getElementById('games');
+
+
+  /* Respond to UI events */
+
+  // Create and join actions
+
+  createButton.addEventListener('click', () => {
+    socket.emit('create');
+  });
+
+  joinForm.addEventListener('submit', e => {
+    const game = join.querySelector('input').value;
+    document.location.hash = game;
+    e.preventDefault();
+  });
+
+  // Browser navigation stuff
+
+  function leaveGame() {
+    if ( !currentGame ) return;
+    socket.emit('leave', currentGame);
+    leaveCallback();
+  }
+
+  function navigate() {
+    let hash = window.location.hash;
+    if (hash.startsWith('#')) hash = hash.substr(1);
+    if ( hash == '' ) {
+      // Back to front porch "page"
+      document.querySelectorAll('.page').forEach(e => e.classList.remove('active'));
+      document.getElementById('front-porch').classList.add('active');
+      leaveGame();
+    } else {
+      // Go to this game
+      socket.emit('join', hash);
+    }
+  }
+
+  window.addEventListener('beforeunload', () => leaveGame());
+  window.addEventListener('hashchange', () => navigate());
+  navigate(); // Also trigger on page load
+
+
+  /* Respond to server events */
+
+  socket.on('list', list => {
+    gamesList.innerHTML = list.map(game => `<li><a href='#${game}'>${game}</a></li>`)
+                              .join('');
+  });
+
+  socket.on('game-joined', game => {
+    currentGame = game;
+    document.location.hash = game;
+
+    document.querySelectorAll('.page').forEach(e => e.classList.remove('active'));
+    document.getElementById('game').classList.add('active');
+    document.querySelector('#game h2').innerHTML = `"Game" ${game}`;
+    socket.emit('players');
+
+    joinCallback(game);
+  });
+
+  socket.on('players', players => {
+    playersList.innerHTML = players.map(p => `<li>${p}</li>`)
+                                   .join('');
+  });
+
+  socket.on('err', msg => {
+    alert(msg);
+  });
+
+  // Request game list on first page load
+
+  socket.emit('list');
+
+  return () => currentGame;
+}
--- server/events-api/game.js
+++ server/events-api/game.js
@@ -6,32 +6,53 @@ module.exports = async io => {
 
   io.on('connection', socket => {
 
-    socket.on('create', config => {
-      const game = uuid();
+    socket.on('create', async () => {
+      const game = uuid().substring(0,6).toUpperCase();
       games.push(game);
       socket.join(game);
-      socket.emit('game-joined', game);
       console.log("Created game", game);
+
+      // Tell client it joined this new game
+      socket.emit('game-joined', game);
+
+      // Tell everyone the new game exists
+      io.emit('list', games);
+
+      // Tell everyone in this game who the players are
+      io.in(game).emit('players', await playersInGame(game));
     });
 
-    socket.on('join', game => {
+    socket.on('join', async game => {
+      if ( !games.includes(game) ) {
+        return socket.emit('err', 'Sorry, this game doesn\'t exist (anymore)');
+      }
+
       socket.join(game);
+
+      // Tell client it joined this existing game
       socket.emit('game-joined', game);
+
+      // Tell everyone in this game who the players are
+      io.in(game).emit('players', await playersInGame(game));
     });
 
     socket.on('list', () => {
       socket.emit('list', games);
     });
 
-    socket.on('leave', game => {
+    socket.on('leave', async game => {
       socket.leave(game);
-      io.in(game).clients((error, clients) => {
-        if (error) throw error;
-        if ( games.includes(game) && clients.length == 0 ) {
-          games.splice(games.indexOf(game), 1);
-          console.log("Cleaned up game", game);
-        }
-      });
+
+      // Tell everyone in this game who the players are
+      const players = await playersInGame(game);
+      io.in(game).emit('players', players);
+
+      if ( games.includes(game) && players.length == 0 ) {
+        games.splice(games.indexOf(game), 1);
+        // Tell everyone the game has disappeared
+        io.emit('list', games);
+        console.log("Cleaned up game", game);
+      }
     });
 
     socket.on('message', ({game, message}) => {
@@ -40,4 +61,12 @@ module.exports = async io => {
 
   });
 
+  function playersInGame(game) {
+    return new Promise((resolve, reject) => {
+      io.in(game).clients((error, clients) => {
+        if (error) reject(error);
+        resolve(clients);
+      });
+    });
+  }
 }
