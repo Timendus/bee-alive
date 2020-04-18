c6dc471f7904c70c53f03b37d25104f0e26a91e1
Bob van der Linden <bobvanderlinden@gmail.com>
2020-04-18 17:07:59 +0200

    game: handle connect, disconnect and input of players

--- shared/bee-game.js
+++ shared/bee-game.js
@@ -1,19 +1,48 @@
 class BeeGame {
   init() {
     return {
-      frame: 0
-    }
+      frame: 0,
+      players: [],
+    };
   }
 
   update(state, events) {
-    // Handle game state updating.
-    return {
-      ...state,
-      frame: state.frame + 1,
-    };
+    return events.reduce(updateState, { ...state, frame: state.frame + 1 });
   }
 }
 
-module.exports = {
-  BeeGame
+function updateState(state, event) {
+  return {
+    ...state,
+    players: updatePlayers(state.players, event),
+  };
 }
+
+function updatePlayers(players, event) {
+  switch (event.type) {
+    case "connect":
+      return [...players, { id: event.clientid, x: 0, y: 0 }];
+    case "disconnect":
+      return players.filter((player) => player.id !== event.clientid);
+    case "game-input":
+      return players.map((player) =>
+        event.clientid === player.clientid
+          ? updatePlayerInput(player, input)
+          : player
+      );
+    default:
+      return players;
+  }
+}
+
+function updatePlayerInput(player, input) {
+  return {
+    ...player,
+    x: player.x + (input === "left" ? -1 : input === "right" ? 1 : 0),
+    y: player.y + (input === "up" ? -1 : input === "down" ? 1 : 0),
+  };
+}
+
+module.exports = {
+  BeeGame,
+};
