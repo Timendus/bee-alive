c490f03b00121772a56eaa98aa774418b7b43bd8
Anne-Greeth van Herwijnen <anne-greeth.vanherwijnen@nedap.com>
2020-04-19 20:12:21 +0200

    Adds BeeHives to the canvas

--- client/textures.js
+++ client/textures.js
@@ -1,16 +1,20 @@
 const team0 = {
   player: new Image(),
-  boid: new Image()
+  boid: new Image(),
+  hive : new Image()
 }
 team0.player.src = 'images/team1_bee.png';
 team0.boid.src = 'images/team1_boid.png';
+team0.hive.src = 'images/beehive-blue-cross.png';
 
 const team1 = {
   player: new Image(),
-  boid: new Image()
+  boid: new Image(),
+  hive : new Image()
 }
 team1.player.src = 'images/team2_bee.png';
 team1.boid.src = 'images/team2_boid.png';
+team1.hive.src = 'images/beehive-pink-cross.png';
 
 module.exports = {
   team0,
